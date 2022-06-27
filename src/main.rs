75909fcf307cf5f7aafeeb6b4ee361d754062395
stavrogyn <nikitakrugovoy@gmail.com>
2022-06-28 02:20:44 +0400

    feat(health-check): create handler and route for health checking

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,14 @@
-use env_logger::Env;
-use actix_web::{ middleware::Logger, web, App, HttpRequest, HttpServer, Responder};
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
 
-async fn greet(req: HttpRequest) -> impl Responder {
-    let name = req.match_info().get("name").unwrap_or("World"); format!("Hello {}!", &name)
+async fn health_check() -> impl Responder {
+    HttpResponse::Ok()
 }
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-
-    env_logger::init_from_env(Env::default().default_filter_or("info"));
-
-    HttpServer::new(|| { App::new()
-        .route("/", web::get().to(greet))
-        .route("/{name}", web::get().to(greet))
-        .wrap(Logger::default())
-        .wrap(Logger::new("%a %{User-Agent}i"))
+    HttpServer::new(|| {
+        App::new()
+            .route("/health_check", web::get().to(health_check))
     })
         .bind("127.0.0.1:8000")?
         .run()
