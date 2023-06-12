0d708f9e4a7c322997871a59ca4e7fc84c634b6a
stavrogyn <nikitakrugovoy@gmail.com>
2023-06-12 23:09:24 +0300

    stuct: move routes to separate module + use creates + create config module

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
-use zero2prod::run;
 use std::net::TcpListener;
+use zero2prod::startup::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    let listener = TcpListener::bind("127.0.0.1:0")
+        .expect("Failed to bind random port");
     run(listener)?.await
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,4 +1,5 @@
 use std::net::TcpListener;
+use zero2prod::startup::run;
 
 #[tokio::test]
 async fn health_check_works() {
@@ -17,7 +18,7 @@ fn spawn_app() -> String {
     let listener = TcpListener::bind("127.0.0.1:0")
         .expect("Failed to bind random port");
     let port = listener.local_addr().unwrap().port();
-    let server = zero2prod::run(listener).expect("Failed to bind address"); let _ = tokio::spawn(server);
+    let server = run(listener).expect("Failed to bind address"); let _ = tokio::spawn(server);
     format!("http://127.0.0.1:{}", port)
 }
 
